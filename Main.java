67edf9efdf421f05a4b913e0bf14556f3b7e3369
100433273EF <100433273@ea.edin.sch.uk>
2021-01-13 15:21:16 +0000

    I did it

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     //Step 1: boolean valid set to false
+    boolean valid = false;
+    String password = "";
+    int length = 0;
     //Step 2: start conditional loop while valild is false
+    while (valid == false) {
+        password = Keyboard.getText("Please enter your password.");
+        length = password.length();
+        char firstLetter= password.charAt(0);
+        char lastLetter = password.charAt(length - 1);
+        int firstValue = (int) firstLetter;
+        int lastValue = (int) lastLetter;
+        if ((firstValue >= 40 && firstValue <= 42) && (lastValue >= 65 && lastValue <= 90) && (length > 7)) {
+          valid = true;
+        } else {
+          System.out.println("Error: password must be at least 8 characters, with ()* to start and a capital at the end.");
+        }
+    }
+    System.out.println("Your password meets the strength requirements.");
     //Step 3: ask user to enter password
     //Step 4: set character firstLetter to the value of the first letter of password
     //Step 5: set character lastLetter to the value of the last letter of password
